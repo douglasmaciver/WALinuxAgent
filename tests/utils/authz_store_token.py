4f8e037c01bbc9090450eab66a83b7f3140cc93b
douglasmaciver <douglas.maciver@microsoft.com>
2022-01-16 11:53:39 -0800

    improved filename handling for authz_store_token

--- tests/utils/authz_store_token.py
+++ tests/utils/authz_store_token.py
@@ -21,27 +21,28 @@ import uuid
 
 AUTHZTOKEN_INCOMING_FILE_SUFFIX = ".authzap"
 
-def store_token_from_str(token: str, filename_override: str = None) -> str:
+def store_token_from_str(authz_dir: str, token: str, filename_override: str = None) -> str:
     """Store a token given a string"""
-    random_filename = str(uuid.uuid4())
+    # TODO: improve resilience
+    short_filename = token[:16]
     if not filename_override:
-        filename = random_filename + AUTHZTOKEN_INCOMING_FILE_SUFFIX 
+        filename = short_filename + AUTHZTOKEN_INCOMING_FILE_SUFFIX 
     else:
         filename = filename_override + AUTHZTOKEN_INCOMING_FILE_SUFFIX  
 
-    # TODO: hard-coded "authz" is brittle. Read it from config.
-    token_file_path = os.path.join(os.getcwd(), "authz", filename)
+    token_file_path = os.path.join(authz_dir, filename)
     with open(token_file_path, "w") as token_file_out:
         token_file_out.write(token)
     return token_file_path
 
 def main() -> int:
     parser = argparse.ArgumentParser(description='authz token services')
+    parser.add_argument('authz_dir', nargs=1, type=str,
+                        help='authz directory')
     parser.add_argument('token_str', nargs=1, type=str,
                         help='token string')
-
     args = parser.parse_args()
-    store_token_from_str(args.token_str[0])
+    store_token_from_str(args.authz_dir[0], args.token_str[0])
     return 0
 
 if __name__ == '__main__':
